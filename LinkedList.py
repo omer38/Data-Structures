7c92371a55c25869bf42eb18db3185503faac5c7
Ömer Tuğrul <omertugrul24@gmail.com>
2023-08-08 18:52:37 +0300

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 class Node:
     def __init__(self,value):
         self.value = value
@@ -32,7 +39,7 @@ class LinkedList:
             return None
         temp = self.head
         pre = self.head
-        while self.head is not None:
+        while temp.next is not None:
             pre = temp
             temp = temp.next
         self.tail = pre
@@ -80,9 +87,51 @@ class LinkedList:
             return True
         return False
     
-my_linked_list = LinkedList(11)
+    def insert(self,index,value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0 :
+            return self.prepend(value)
+        if index == self.length :
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+            
+        
+    
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
 my_linked_list.append(3)
-my_linked_list.append(23)
-my_linked_list.append(7)
-my_linked_list.set_value(1,4)
+my_linked_list.append(4)
+my_linked_list.reverse()
 my_linked_list.print_list()
