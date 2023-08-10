c89cc6ff67d37620138c65e48fa88f58095d3945
Ömer Tuğrul <omertugrul24@gmail.com>
2023-08-10 13:07:37 +0300

    Create Stack&Queue.py

---
+++ Stack&Queue.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 17:20:43 2023
+
+@author: user
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class Stack:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+    
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def push(self,value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+    
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
+    
+class Queue:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+        
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+    
+        
+    
+my_stack = Queue(7)
+my_stack.enqueue(23)
+my_stack.dequeue()
+
+my_stack.print_queue()
+
+
+
